738e2472d8da5557325b53fe87d4bab7a7093f1d
punit <punit.sureka@talview.com>
2023-05-02 19:03:39 +0530

    mvp

--- modules/image_to_txt.py
+++ modules/image_to_txt.py
@@ -2,20 +2,22 @@ import os
 import pytesseract
 from PIL import Image
 
+
 def image_to_text(input_dir, output_dir):
     try:
+        image_files = [
+            f for f in os.listdir(input_dir) if f.endswith(".jpg") or f.endswith(".png")
+        ]
+        output_file = os.path.join(output_dir, "output.txt")
 
-        image_files = [f for f in os.listdir(input_dir) if f.endswith('.jpg') or f.endswith('.png')]
-        output_file = os.path.join(output_dir, 'output.txt')
-
-        with open(output_file, 'w') as f:
+        with open(output_file, "w") as f:
             for image_file in image_files:
                 image_path = os.path.join(input_dir, image_file)
                 image = Image.open(image_path)
                 text = pytesseract.image_to_string(image)
                 f.write(text)
 
-        print('Text extracted from images and saved to directory:', output_dir)
+        print("Text extracted from images and saved to directory:", output_dir)
 
     except:
-        print('Error in converting image to text:', image_path)
+        print("Error in converting image to text:", image_path)
